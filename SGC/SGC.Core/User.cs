141ef85914068dbfcfd3d07946afdaea9addaca8
LaoSabino <junior.sabino@gmail.com>
2017-05-13 13:13:25 -0300

    add items

--- SGC/SGC.Core/User.cs
+++ SGC/SGC.Core/User.cs
@@ -8,5 +8,14 @@ namespace SGC.Core
 {
     public class User
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
     }
 }
